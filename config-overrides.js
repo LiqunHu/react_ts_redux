633e239bece764f987c4259a5ff3abaae22c9deb
LiqunHu <huliquns@126.com>
2020-08-14 16:53:27 +0800

    增加ant按需加载

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,10 @@ const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
 const path = require("path");
 
 const pkg = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        style: 'css',
+    }),
     addWebpackAlias({
         // add your alias
         ['@']: path.resolve(__dirname, './src')
